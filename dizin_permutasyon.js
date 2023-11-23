c2a52ae62ae2ab194b609063e0ad760fd10132fe
burak <vburak9029@gmail.com>
2023-11-23 22:25:42 +0300

    changed

--- dizin_permutasyon.js
+++ dizin_permutasyon.js
@@ -1,14 +1,13 @@
 
 //permutations([1, 33, 5]);
 /*
-abi array permutasyonunu şu şekilde gerçekleştiğini görüyorum düşünüyorum: eğer diyor array 2 veya daha az elemanlı olursa
-iki eleman sayısını 2 eleman olarak varsay ve arrayın kendisi ve arr 2. elemanı ve ilk elemanını array olarak koymaya devam et diyor vee array aşağıdaki işlemlerden sonra
-reduce yani sadeleştir diyor diye anladım başkanım. acc nedir anlamadım ancak fonksiyon içinde tanımlanan değişken gibi geldi. ama enson concat kısmında iki farklı erray 
-ancak aynı türdeki dataları birleştirdiğini biliyorum ancak burada en son permutasyon da ne yapmak istediğini anlamadım.
+
 
 */
 const permutations = arr => {
+  //dizin eleman sayısı 2 den düşük olduğunda yeni dizin arr , arrayın 1. ve 2. elemanı oluşturur.
     if (arr.length <= 2) return arr.length === 2 ? [arr, [arr[1], arr[0]]] : arr;
+   //
     return arr.reduce(
       (acc, item, i) =>
         acc.concat(

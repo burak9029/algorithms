9dc055e357256761d6273d9eff7fe50f05c371a8
burak <vburak9029@gmail.com>
2023-11-23 23:06:25 +0300

    changed

--- string_permutation.js
+++ string_permutation.js
@@ -4,16 +4,16 @@ let findPermutations = (string) => {
     if (!string || typeof string !== "string"){
       return "Please enter a string"
     }
-  
+  //girdi tipi string değilse ya da girilen string değilse "please enter a string" uyarısı alırız.
     if (!!string.length && string.length < 2 ){
       return string
     }
-  
+  //string olmayanın eleman miktarı ile string olanın eleman miktarı 2 den az ise cevap verilen string olacaktır.
     let permutationsArray = [] 
      
     for (let i = 0; i < string.length; i++){
       let char = string[i]
-  
+  //char varyantı string i ye eşit ve index of char i ye eşit olmazsa kodun çalışmasına devam dilir ve 
       if (string.indexOf(char) != i)
       continue
   
